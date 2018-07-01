8d95ce36d53545cdcf0c0abb916326ad60cb498e
thorout <thorout@gmail.com>
2018-07-01 15:19:36 +0300

    add some tests

--- TDDEducation/Soundex.h
+++ TDDEducation/Soundex.h
@@ -1,11 +1,20 @@
 #pragma once
 #include <string>
 
+
 using std::string;
 
 class Soundex
 {
 public:
-	string encode(string word) /*{ return word; }*/;
+	string encode(const string& word) const;
+private:
+	string head(const string& word) const;
+	string tail(const string& word) const;
+	string encodedDigits(const string& word) const;
+	bool isComplete(const std::string& encoding) const;
+	string zeroPad(const string& word) const;
+public:
+	string encodedDigit(char letter) const;
 };
 
